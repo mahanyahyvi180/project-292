86ba3586717aa5b1fc6e11a989b95d0d931f8ae9
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-12-31 22:26:10 +0330

    Add files via upload

---
+++ ConsoleApp154/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var letters = "abcdefghijklmabcdefghijklm";
+
+            Console.WriteLine("Substring from index 20 to end is" + $"\"{letters.Substring(20)}\"");
+            Console.WriteLine("Substring from index 0 of length 6 is" + $"\"{letters.Substring(0, 6)}\"");
+
+            Console.ReadLine();
+        }
+    }
+}
